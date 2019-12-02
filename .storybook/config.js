60e9801c838717726b8c3af5fc1d59f37c804cd3
panpan <panpan31415@gmail.com>
2019-12-02 13:13:14 +0100

    save work on chanpter 2 simple components

---
+++ .storybook/config.js
@@ -0,0 +1,15 @@
+
+
+// automatically import all files ending in *.stories.js
+// configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+import '../src/index.css';
+const req = requireContext('../src', true, /\.stories.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
